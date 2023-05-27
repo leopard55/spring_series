9372625f8eafa5e0e942714ed71fa09b653acc6c
leoaprd <806005126@qq.com>
2023-05-27 23:44:57 +0800

    修改

--- spring_ioc_test01/src/main/java/com/itheima/handlers/HaohaoBeanDefinitionParser.java
+++ spring_ioc_test01/src/main/java/com/itheima/handlers/HaohaoBeanDefinitionParser.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 public class HaohaoBeanDefinitionParser implements BeanDefinitionParser {
+    @Override
     public BeanDefinition parse(Element element, ParserContext parserContext) {
         //注入一个BeanPostProcessor
         BeanDefinition beanDefinition = new RootBeanDefinition();
